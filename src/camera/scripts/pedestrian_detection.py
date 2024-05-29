83a8ab9d12c3e02f2a31eb3fec81ab4645539f98
HugoChlt <hugo.choulot@utbm.fr>
2024-05-29 16:13:43 +0200

    refactoring code + lidar pedestrian detection

--- src/camera/scripts/pedestrian_detection.py
+++ src/camera/scripts/pedestrian_detection.py
@@ -18,6 +18,8 @@ class ObstacleDetector:
         self.rate = rospy.Rate(10)  # 10 Hz
         self.bridge = CvBridge()
 
+        self.boxes = []
+
         # Use message_filters to subscribe to color and depth image topics
         self.image_sub = Subscriber("/camera/color/image_raw", Image)
         self.depth_sub = Subscriber("/camera/depth/image_rect_raw", Image)
@@ -30,10 +32,18 @@ class ObstacleDetector:
         self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.brake_pub = rospy.Publisher('/emergency_brake', Int32, queue_size=1)
         self.marker_pub = rospy.Publisher('pedestrian_markers', MarkerArray, queue_size=10)
+
+        # Load YOLO
+        self.net = cv2.dnn.readNet("/home/hugoc/yolov3.weights", "/home/hugoc/RO50_project_ws/src/camera/scripts/yolov3.cfg")
+        with open("/home/hugoc/RO50_project_ws/src/camera/scripts/coco.names", "r") as f:
+            self.classes = [line.strip() for line in f.readlines()]
+
+        layer_names = self.net.getLayerNames()
+        self.output_layers = [layer_names[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
         
         self.depth_image = None
 
-        rospy.loginfo("Obstacle and Pedestrian Detector Initialized")
+        # rospy.loginfo("Obstacle and Pedestrian Detector Initialized")
         self.rate.sleep()
 
     def synced_callback(self, color_data, depth_data, lidar_data):
@@ -49,74 +59,30 @@ class ObstacleDetector:
                 pass  # No scaling needed for 32-bit float encoding
 
             self.depth_image = depth_image_cv
-            self.process_image()
-            self.process_lidar(lidar_data)
-            #self.process_image_rgb(color_image)
+            self.process_image_depth()
+            # self.process_lidar(lidar_data)
             self.detect_humans(color_image)
 
-            rospy.loginfo("Color image shape: %s", color_image.shape)
+            # rospy.loginfo("Color image shape: %s", color_image.shape)
         except CvBridgeError as e:
             rospy.logerr("CvBridge Error: {0}".format(e))
+    
+    def save_image(self, image, image_path):
+        # Save the RGB image with detections
+        save_filename = image_path
+        save_path = os.path.join(os.getcwd(), save_filename)
+        cv2.imwrite(save_path, image)
 
-    def process_image(self):
+    def process_image_depth(self):
         if self.depth_image is not None:
             # Detect obstacles within 2 meters
             self.depth_image = self.depth_image[:, 104:744]
-            self.depth_image = np.where(self.depth_image < 2.0, 10, self.depth_image)
+            self.depth_image = np.where(self.depth_image < 0.5, 10, self.depth_image)
             self.depth_image = median_filter(self.depth_image, size=3)
-            self.depth_image[self.depth_image < 2.0] = 10
+            self.depth_image[self.depth_image < 0.5] = 10
 
             save_filter_image = (self.depth_image * 25).astype(np.uint8)
             cv_mat = cv2.cvtColor(save_filter_image, cv2.COLOR_GRAY2BGR)
-
-            # # Save the image in the current working directory
-            # save_filename = "filter_image_depth.png"
-            # save_path = os.path.join(os.getcwd(), save_filename)
-
-            # # Attempt to save the image and log success or failure
-            # if cv2.imwrite(save_path, cv_mat):
-            #     rospy.loginfo(f"Depth image saved successfully at: {save_path}")
-            # else:
-            #     rospy.logerr(f"Failed to save depth image at: {save_path}")
-
-            count_images = np.sum(self.depth_image < 2)
-            rospy.loginfo(self.depth_image.shape)
-            rospy.loginfo(self.depth_image)
-            rospy.loginfo(count_images)
-
-            obstacle_mask = (self.depth_image < 2.0)  # Threshold for obstacle detection
-            obstacle_detected = np.any(obstacle_mask)
-
-            if obstacle_detected:
-                rospy.loginfo("Obstacle detected! Stopping the robot.")
-                self.stop_robot()
-    
-    def process_image_rgb(self, color_image):
-
-        # hog = cv2.HOGDescriptor()
-        # hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-        # (regions, weights) = hog.detectMultiScale(color_image, winStride=(4,4), padding=(8,8), scale = 1.05)
-        
-        # rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in regions])
-        # pick = non_max_suppression(rects, probs=None, overlapThresh = 0.65)
-
-        # for(x, y, x2, y2) in pick:
-        #     cv2.rectangle(color_image, (x, y), (x2, y2), (0, 0, 255), 2)
-
-        body=cv2.CascadeClassifier('/home/hugoc/RO50_project_ws/src/camera/scripts/haarcascade_fullbody.xml')
-        detection=body.detectMultiScale(color_image,scaleFactor=1.05,minSize=(50,50))
-        for (x,y,w,h) in detection:
-            cv2.rectangle(color_image,(x,y),(x+w,y+h),(0,0,255),2)
-        
-        # save_filename = "rgb_image.png"
-        # save_path = os.path.join(os.getcwd(), save_filename)
-
-        # # Attempt to save the image and log success or failure
-        # if cv2.imwrite(save_path, color_image):
-        #     rospy.loginfo(f"Depth image saved successfully at: {save_path}")
-        # else:
-        #     rospy.logerr(f"Failed to save depth image at: {save_path}")
 
     def process_lidar(self, data):
         ranges = np.array(data.ranges)
@@ -133,21 +99,26 @@ class ObstacleDetector:
         start_index = center_index - indices_range // 2
         end_index = center_index + indices_range // 2
 
-        # Filter the ranges to include only the desired 130 degree field of view
+        # Filter the ranges to include only the desired 120 degree field of view
         filtered_ranges = np.concatenate((ranges[start_index:], ranges[:end_index])) if start_index < 0 else ranges[start_index:end_index]
 
         distance_threshold = 0.2  # Distance threshold for clustering in meters
-        target_distance = 2.0     # Target distance for pedestrian detection
+        target_distance = 4.0     # Target distance for pedestrian detection
+
+        # Check for any object within 1 meter
+        if np.any(filtered_ranges <= 1.0):
+            rospy.loginfo("Obstacle detected within 1 meter! Stopping the robot.")
+            self.stop_robot()
+            return  # Exit the function early to immediately stop the robot
+        
         clusters = self.euclidean_clustering(filtered_ranges, angle_increment, distance_threshold)
         
-        min_size = 5   # Minimum number of points in a cluster to be considered a pedestrian
+        min_size = 3   # Minimum number of points in a cluster to be considered a pedestrian
         max_size = 50  # Maximum number of points in a cluster to be considered a pedestrian
         pedestrian_clusters = self.detect_pedestrians(clusters, min_size, max_size, target_distance, distance_threshold)
-        
-        if pedestrian_clusters:
-            rospy.loginfo("Pedestrians detected: %d", len(pedestrian_clusters))
-            self.publish_markers(pedestrian_clusters)
-            self.stop_robot()
+
+        if pedestrian_clusters and len(self.boxes) > 0:
+            rospy.loginfo("Pedestrians detected:")
 
     def euclidean_clustering(self, ranges, angle_increment, distance_threshold):
         clusters = []
@@ -176,32 +147,42 @@ class ObstacleDetector:
                     pedestrian_clusters.append((cluster, mean_distance))
         return pedestrian_clusters
     
-    def detect_humans(self, image):
-        # Load YOLO
-        net = cv2.dnn.readNet("/home/hugoc/yolov3.weights", "/home/hugoc/RO50_project_ws/src/camera/scripts/yolov3.cfg")
-        with open("/home/hugoc/RO50_project_ws/src/camera/scripts/coco.names", "r") as f:
-            classes = [line.strip() for line in f.readlines()]
+    def detect_box_hog(self, image):
+        hog = cv2.HOGDescriptor()
+        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-        layer_names = net.getLayerNames()
-        output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        (regions, weights) = hog.detectMultiScale(image, winStride=(4,4), padding=(8,8), scale = 1.05)
+        
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in regions])
+        pick = non_max_suppression(rects, probs=None, overlapThresh = 0.65)
 
+        self.boxes = []
+        for(x, y, x2, y2) in pick:
+            self.boxes.append(x, y , abs(x2-x), abs(y2-y))
+
+    def detect_box_cascade(self, image):
+        body=cv2.CascadeClassifier('/home/hugoc/RO50_project_ws/src/camera/scripts/haarcascade_fullbody.xml')
+        detection=body.detectMultiScale(image,scaleFactor=1.05,minSize=(50,50))
+        self.boxes = [box for box in detection]
+
+    def detect_box(self, image):
         height, width, channels = image.shape
 
         # Detecting objects
         blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-        net.setInput(blob)
-        outs = net.forward(output_layers)
+        self.net.setInput(blob)
+        outs = self.net.forward(self.output_layers)
 
         # Showing information on the screen
         class_ids = []
         confidences = []
-        boxes = []
+        self.boxes = []
         for out in outs:
             for detection in out:
                 scores = detection[5:]
                 class_id = np.argmax(scores)
                 confidence = scores[class_id]
-                if confidence > 0.5 and class_id == 0:  # Only detecting humans (class_id == 0 for person)
+                if confidence > 0.3 and class_id == 0:  # Only detecting humans (class_id == 0 for person)
                     center_x = int(detection[0] * width)
                     center_y = int(detection[1] * height)
                     w = int(detection[2] * width)
@@ -211,28 +192,55 @@ class ObstacleDetector:
                     x = int(center_x - w / 2)
                     y = int(center_y - h / 2)
 
-                    boxes.append([x, y, w, h])
+                    self.boxes.append([x, y, w, h])
                     confidences.append(float(confidence))
                     class_ids.append(class_id)
 
-        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        indexes = cv2.dnn.NMSBoxes(self.boxes, confidences, 0.5, 0.4)
+        box_temp = []
+        for i in range(len(self.boxes)):
+            if i in indexes and w > 5 and h > 5 and y > 0 and x > 0:
+                box_temp.append(self.boxes[i])
+        self.boxes=[]
+        self.boxes = box_temp        
 
-        for i in range(len(boxes)):
-            if i in indexes:
-                x, y, w, h = boxes[i]
-                label = str(classes[class_ids[i]])
-                confidence = confidences[i]
-                color = (0, 255, 0)  # Green color for bounding box
-                cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-                cv2.putText(image, label + " " + str(round(confidence, 2)), (x, y + 30), cv2.FONT_HERSHEY_PLAIN, 3, color, 3)
-        
-        # Save the RGB image with detections
-        save_filename = "rgb_image_yolo.png"
-        save_path = os.path.join(os.getcwd(), save_filename)
-        if cv2.imwrite(save_path, image):
-            rospy.loginfo(f"RGB image saved successfully at: {save_path}")
-        else:
-            rospy.logerr(f"Failed to save RGB image at: {save_path}")
+    def detect_humans_distance(self) :
+        distance = []
+        for i in range(len(self.boxes)):
+            x, y, w, h = self.boxes[i]
+            array = self.depth_image[y:y+h, x:x+w].flatten()
+            pixel_index = int(w*h*0.1)
+            if len(array) <= 0:
+                distance.append(100)
+                continue
+            smallest_values = np.partition(array, pixel_index)[:pixel_index]
+            if len(smallest_values) <= 0:
+                distance.append(100)
+                continue
+            smallest_values_sorted = np.sort(smallest_values)
+
+            distance.append(smallest_values_sorted[-1])
+            
+            # Stop the robot if the average distance is less than 2 meters
+            if smallest_values_sorted[-1] < 2.0:
+                self.stop_robot()
+    
+        return distance
+
+    def draw_boxes(self, image, distances):
+        for i in range(len(self.boxes)):
+            x, y, w, h = self.boxes[i]
+            color = (0, 255, 0)  # Green color for bounding box
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(image, "human" + str(distances[i]), (x, y + 30), cv2.FONT_HERSHEY_PLAIN, 3, color, 3)
+        return image
+
+    def detect_humans(self, image):
+
+        self.detect_box(image)
+        distances = self.detect_humans_distance()
+        boxes_image = self.draw_boxes(image, distances)
+        self.save_image(boxes_image, "image_box_yolo.png")
 
     def publish_markers(self, pedestrian_clusters):
         marker_array = MarkerArray()
